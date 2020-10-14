15dad7746650ed1c8d40e764d66f5efbf18a5ac5
Gordon Grant <gordon.grant2@capita.com>
2020-10-14 17:09:27 +0100

    initial customs for Macs

---
+++ personal/personal.el
@@ -0,0 +1,23 @@
+;; set cmd/opt as meta/super
+(when (string= system-type "darwin")
+  (setq mac-option-key-is-meta nil)
+  (setq mac-option-key-is-super t)
+  (setq mac-command-key-is-super nil)
+  (setq mac-command-key-is-meta t)
+  (setq mac-command-modifier 'meta)
+  (setq mac-option-modifier 'super))
+
+(when (string= system-type "darwin")
+  ;;
+  ;; on mac use GNU ls from coreutils
+  ;;
+  (setq dired-use-ls-dired t
+        insert-directory-program "/usr/local/bin/gls"
+        dired-listing-switches "-aBhl --group-directories-first"))
+
+(when (memq window-system '(mac ns x))
+  (setq exec-path-from-shell-variables '("PATH" "MANPATH" "VAGRANT_DEFAULT_PROVIDER"))
+                                        ;					 "VAGRANT_VMWARE_CLONE_DIRECTORY"
+                                        ;					 "PAGER"
+                                        ;					 "PROMPT_COMMAND"))
+  (exec-path-from-shell-initialize))
