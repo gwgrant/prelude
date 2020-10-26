5053735262926e7a88bd7ea36fbcc23b87ebc905
Gordon Grant <gordon.grant2@capita.com>
2020-10-26 10:57:07 +0000

    customise var

--- personal/custom.el
+++ personal/custom.el
@@ -6,6 +6,7 @@
  '(package-selected-packages
    '(helm-git-grep helm-ls-git helm-rg dockerfile-mode treemacs-icons-dired treemacs-magit treemacs-projectile treemacs use-package terraform-doc company-terraform multi-vterm org desktop+ indent-tools go-mode yaml-mode company-anaconda anaconda-mode lsp-ui company-lsp json-mode js2-mode rainbow-mode elisp-slime-nav rainbow-delimiters company helm-ag helm-descbinds helm-projectile helm exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number nlinum move-text magit projectile imenu-anywhere hl-todo guru-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major crux browse-kill-ring anzu ag ace-window))
  '(treemacs-width 50)
+ '(vterm-max-scrollback 50000)
  '(whitespace-line-column 140))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
