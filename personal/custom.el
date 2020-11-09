9a7b50c92db70505e4ad0852650f1c983ac44c4f
Gordon Grant <gordon.grant@umask.com>
2020-11-09 14:01:27 +0000

    updated

--- personal/custom.el
+++ personal/custom.el
@@ -4,11 +4,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(eterm-256color helm-git-grep helm-ls-git helm-rg dockerfile-mode treemacs-icons-dired treemacs-magit treemacs-projectile treemacs use-package terraform-doc company-terraform multi-vterm org desktop+ indent-tools go-mode yaml-mode company-anaconda anaconda-mode lsp-ui company-lsp json-mode js2-mode rainbow-mode elisp-slime-nav rainbow-delimiters company helm-ag helm-descbinds helm-projectile helm exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number nlinum move-text magit projectile imenu-anywhere hl-todo guru-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major crux browse-kill-ring anzu ag ace-term))
- '(window-mode-hook '(eterm-256color-mode desktop+--term-mode-hook))
+   '(helm-git-grep helm-ls-git helm-rg dockerfile-mode treemacs-icons-dired treemacs-magit treemacs-projectile treemacs use-package terraform-doc company-terraform multi-vterm org desktop+ indent-tools go-mode yaml-mode company-anaconda anaconda-mode lsp-ui company-lsp json-mode js2-mode rainbow-mode elisp-slime-nav rainbow-delimiters company helm-ag helm-descbinds helm-projectile helm exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number nlinum move-text magit projectile imenu-anywhere hl-todo guru-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major crux browse-kill-ring anzu ag ace-term))
  '(treemacs-width 50)
  '(vterm-max-scrollback 50000)
- '(whitespace-line-column 140))
+ '(whitespace-line-column 140)
+ '(window-mode-hook (desktop+--term-mode-hook)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
