157c6c5aba79b3c81be64768ea8f294faeea7dd2
Gordon Grant <gordon.grant@umask.com>
2020-11-19 12:57:14 +0000

    customs

--- personal/custom.el
+++ personal/custom.el
@@ -3,9 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
  '(mini-frame-show-parameters '((left . 0.5) (top . 0) (width . 1.0) (height . 1)))
  '(package-selected-packages
    '(mini-frame helm-posframe posframe helm-git-grep helm-ls-git helm-rg dockerfile-mode treemacs-icons-dired treemacs-magit treemacs-projectile treemacs use-package terraform-doc company-terraform multi-vterm org desktop+ indent-tools go-mode yaml-mode company-anaconda anaconda-mode lsp-ui company-lsp json-mode js2-mode rainbow-mode elisp-slime-nav rainbow-delimiters company helm-ag helm-descbinds helm-projectile helm exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number nlinum move-text magit projectile imenu-anywhere hl-todo guru-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major crux browse-kill-ring anzu ag ace-term))
+ '(size-indication-mode t)
+ '(tool-bar-mode nil)
  '(treemacs-width 50)
  '(vterm-max-scrollback 50000)
  '(whitespace-line-column 140)
