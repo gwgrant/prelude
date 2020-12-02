5eb3314415fb23c2a7a7859a27d31ed554dc0066
Gordon Grant <gordon.grant@umask.com>
2020-12-02 11:27:36 +0000

    add key binding

--- personal/personal.el
+++ personal/personal.el
@@ -40,7 +40,7 @@
 (setq helm-posframe-parameters
       '((left-fringe . 10)
         (right-fringe . 10)
-        (undecorated . nil)
+        (undecorated . t)
         (internal-border-width . 5)
         (min-height . 50)))
 (setq helm-posframe-poshandler #'posframe-poshandler-window-top-center)
@@ -52,3 +52,6 @@
 
 ;; blink the cursor
 (blink-cursor-mode)
+
+;; another key binding for ace-window
+(global-set-key (kbd "C-\'") 'ace-window)
