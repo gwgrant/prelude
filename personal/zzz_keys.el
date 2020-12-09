70afe5db9f5ea7e527df678bcf5195d91049f697
Gordon Grant <gordon.grant@umask.com>
2020-12-09 09:32:07 +0000

    change binding

--- personal/zzz_keys.el
+++ personal/zzz_keys.el
@@ -1,4 +1,4 @@
 (global-set-key (kbd "s-9") 'xah-previous-user-buffer)
 (global-set-key (kbd "s-0") 'xah-next-user-buffer)
 (define-key ctl-x-map "\C-q" 'view-mode)
-(global-set-key (kbd "C-\'") 'ace-window)
+(global-set-key (kbd "s-;") 'ace-window)
